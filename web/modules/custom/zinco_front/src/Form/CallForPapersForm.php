ca6fee21564f0d25e322da0018feb514866b01dc
Camilo Cervantes <cacesa8931@gmail.com>
2026-04-20 10:23:44 -0500

    selector de entidades

--- web/modules/custom/zinco_front/src/Form/CallForPapersForm.php
+++ web/modules/custom/zinco_front/src/Form/CallForPapersForm.php
@@ -69,8 +69,12 @@ class CallForPapersForm extends FormBase
       $view->initHandlers();
 
       // Render the exposed filter form.
-      $exposed_form = $view->display_handler->getPlugin('exposed_form');
-      //$form['filtros_vista'] = $exposed_form->renderExposedForm();
+      // Render the exposed filter form.
+      $exposed_form_render = $view->display_handler->getPlugin('exposed_form')->renderExposedForm();
+      // Strip form wrappers to avoid nested <form> tags which break the main form submit.
+      unset($exposed_form_render['#type']);
+      unset($exposed_form_render['#theme_wrappers']);
+      $form['filtros_vista'] = $exposed_form_render;
       $form['previsualizacion_usuarios'] = [
         '#type' => 'details',
         '#title' => $this->t('Usuarios seleccionados para el envío'),
