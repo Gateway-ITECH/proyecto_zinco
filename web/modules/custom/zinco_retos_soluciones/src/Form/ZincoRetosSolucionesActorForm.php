bfeae40261882f91410d713453e53d816c793901
Camilo Cervantes <cacesa8931@gmail.com>
2026-04-22 10:10:51 -0500

    ajuste reto detalle

--- web/modules/custom/zinco_retos_soluciones/src/Form/ZincoRetosSolucionesActorForm.php
+++ web/modules/custom/zinco_retos_soluciones/src/Form/ZincoRetosSolucionesActorForm.php
@@ -10,29 +10,14 @@ use Drupal\user\Entity\User;
 /**
  * Form controller for the zinco retos soluciones entity edit forms for actors.
  */
-class ZincoRetosSolucionesActorForm extends ZincoRetosSolucionesForm {
+class ZincoRetosSolucionesActorForm extends ZincoRetosSolucionesForm
+{
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    // Si la entidad es nueva y no tiene autores, asignar el actor del usuario actual.
-    if ($this->entity->isNew() && $this->entity->get('field_autores_solucion')->isEmpty()) {
-      $current_user = \Drupal::currentUser();
-      $user_entity = User::load($current_user->id());
-      if ($user_entity && $user_entity->hasField('field_actor') && !$user_entity->get('field_actor')->isEmpty()) {
-        $actor_id = $user_entity->get('field_actor')->target_id;
-        $this->entity->set('field_autores_solucion', [$actor_id]);
-      }
-    }
-
-    return parent::buildForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function save(array $form, FormStateInterface $form_state): int {
+  public function save(array $form, FormStateInterface $form_state): int
+  {
     $result = parent::save($form, $form_state);
 
     $current_user = \Drupal::currentUser();
