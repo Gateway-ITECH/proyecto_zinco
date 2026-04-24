e305d7a68f993b529a4515016a65f6f0dc3c36ca
Camilo Cervantes <cacesa8931@gmail.com>
2026-04-24 09:20:24 -0500

    ajuste cursos

--- web/modules/custom/zinco_front/src/Controller/ZincoController.php
+++ web/modules/custom/zinco_front/src/Controller/ZincoController.php
@@ -1169,7 +1169,12 @@ class ZincoController extends ControllerBase
     // Additional course metadata.
     $data['duracion'] = $node->hasField('field_duracion_horas') ? $node->get('field_duracion_horas')->value : NULL;
     $data['entidad'] = $node->hasField('field_entidad_que_certifica') ? $node->get('field_entidad_que_certifica')->value : NULL;
-    $data['modalidad'] = $node->hasField('field_modalidad_del_curso') ? $node->get('field_modalidad_del_curso')->value : NULL;
+    $data['modalidad'] = NULL;
+    if ($node->hasField('field_modalidad_del_curso') && !$node->get('field_modalidad_del_curso')->isEmpty()) {
+      if ($term = $node->get('field_modalidad_del_curso')->entity) {
+        $data['modalidad'] = $term->label();
+      }
+    }
 
     if ($node->hasField('field_fecha_inicio') && !$node->get('field_fecha_inicio')->isEmpty()) {
       $data['fecha_inicio'] = \Drupal::service('date.formatter')->format(strtotime($node->get('field_fecha_inicio')->value), 'custom', 'd/m/Y');
