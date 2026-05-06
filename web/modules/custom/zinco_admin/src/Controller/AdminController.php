b864ffcb677a9a162df460b84820a64757d5e546
Camilo Cervantes <cacesa8931@gmail.com>
2026-05-06 14:02:59 -0500

    ajuste integracion

--- web/modules/custom/zinco_admin/src/Controller/AdminController.php
+++ web/modules/custom/zinco_admin/src/Controller/AdminController.php
@@ -16,9 +16,20 @@ class AdminController extends ControllerBase {
    *   A render array.
    */
   public function dashboard() {
+    $tables = [
+      'data_view_investigadores' => $this->t('Investigadores'),
+      'data_grupos_investigacion' => $this->t('Grupos de Investigación'),
+      'data_instituciones_academicas' => $this->t('Instituciones Académicas'),
+      'data_programas_academicos' => $this->t('Programas Académicos'),
+      'data_organizaciones_intermedias' => $this->t('Organizaciones Intermedias'),
+      'data_entidades_gobierno' => $this->t('Entidades de Gobierno'),
+      'data_instancias_gobierno' => $this->t('Instancias de Gobierno'),
+    ];
+
     return [
       '#theme' => 'zinco_admin_dashboard',
       '#title' => $this->t('Zinco Admin Dashboard'),
+      '#tables' => $tables,
       '#attached' => [
         'library' => [
           'zinco_admin/zinco_admin.admin_styles',
